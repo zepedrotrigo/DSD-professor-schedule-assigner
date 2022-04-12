eb0ef70c5b9dac34b1c5a160897053b89347f333
zepedrotrigo <josetrigo2001@gmail.com>
2022-04-12 23:07:40 +0100

    updated cells styling

--- frontend/src/components/Cell.js
+++ frontend/src/components/Cell.js
@@ -4,7 +4,15 @@ function Cell(props) {
 
     return (
         <div className={props.extClass}>
-            
+            <div className='hours'>
+                <span>2</span>
+            </div>
+            <div className='text'>
+                <span>PJF</span>
+            </div>
+            <div className='percentage'>
+                <span>100</span>
+            </div>
         </div>
     )
 }
--- frontend/src/views/Home.js
+++ frontend/src/views/Home.js
@@ -6,7 +6,7 @@ import MainPanel from '../components/MainPanel';
 import Cell from '../components/Cell';
 
 function Home() {
-    fetch('http://127.0.0.1:8000')
+    fetch('http://172.18.0.3:8000')
   .then(response => response.json())
   .then(data => console.log(data));
     return (
@@ -16,11 +16,19 @@ function Home() {
             <div className='panel-wrapper'>
                 <SidePanel></SidePanel>
                 <MainPanel>
+                    <Cell />
                     <Cell extClass="cell p"></Cell>
                     <Cell extClass="cell t"></Cell>
                     <Cell extClass="cell tp"></Cell>
                     <Cell extClass="cell lab"></Cell>
                     <Cell extClass="cell outside-activity"></Cell>
+                    <div className='align-cell'>
+                        <Cell />
+                        <Cell extClass="cell sm p"></Cell>
+                        <Cell extClass="cell sm tp"/>
+                        <Cell extClass="cell sm p"></Cell>
+                        <Cell extClass="cell sm p"></Cell>
+                    </div>
                 </MainPanel>
                 <MainPanel>
                     <Cell extClass="cell p"></Cell>
