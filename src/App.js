b2c2d73bd755f3e1ae04c27b48cb10937aa9e605
zepedrotrigo <josetrigo2001@gmail.com>
2022-04-12 16:59:36 +0100

    folder structuring, component structuring

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './views/Home';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	// You can put javascript code here or declare constants!
+	// use {} inside html to access variables
+
+	return (
+		<Router>
+			<div className='App'>
+				<div className='content'>
+					<Routes>
+						<Route path="/" element={<Home />} />
+					</Routes>
+				</div>
+				<footer></footer>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
---
+++ src/views/Home.js
@@ -0,0 +1,12 @@
+import '../App.css';
+
+function Home() {
+    
+    return (
+        <div>
+            <h3>Hello!</h3>
+        </div>
+    )
+}
+
+export default Home;
