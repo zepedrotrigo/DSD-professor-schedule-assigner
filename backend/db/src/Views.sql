6ffa24d6bcb36f653d48ac50704a389e6341e799
zepedrotrigo <josetrigo2001@gmail.com>
2022-04-14 05:45:44 +0100

    added director acronym and students estimate to assigned_classes view

--- backend/db/src/Views.sql
+++ backend/db/src/Views.sql
@@ -1,6 +1,13 @@
-CREATE VIEW assigned_classes AS
-SELECT ucs.acronym AS uc_acronym, ucs.uc_name,
+CREATE VIEW temp_assigned_classes AS
+SELECT ucs.acronym AS uc_acronym, ucs.uc_name, ucs.director, ucs.students_estimate,
 classes.component, classes.class_hours, classes.availability_percent, 
 professors.acronym AS prof_acronym, professors.prof_name
 FROM (classes JOIN professors ON professors.id = classes.prof_id) 
 JOIN ucs ON ucs.id = classes.uc_num;
+
+CREATE VIEW assigned_classes AS
+SELECT uc_acronym, uc_name, professors.acronym AS director_acronym, students_estimate,
+component, class_hours, availability_percent, 
+prof_acronym, temp_assigned_classes.prof_name
+FROM professors JOIN temp_assigned_classes
+WHERE temp_assigned_classes.director=professors.id;
