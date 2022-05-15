b55eafd92c3ca6d62e9da00eb308e5831e8f200e
zepedrotrigo <josetrigo2001@gmail.com>
2022-05-15 18:27:31 +0100

    fixed Views.sql; db collation fixes

--- backend/db/src/Views.sql
+++ backend/db/src/Views.sql
@@ -1,21 +1,25 @@
-CREATE VIEW temp_dsd_main_info AS
+CREATE VIEW temp1 AS
 SELECT ucs.id, ucs.acronym AS uc_acronym, ucs.uc_name, ucs.director, ucs.students_estimate,
 classes.component, classes.class_hours, classes.availability_percent, 
 professors.acronym AS prof_acronym, professors.prof_name
 FROM (classes LEFT OUTER JOIN professors ON professors.id = classes.prof_id) 
 JOIN ucs ON ucs.id = classes.uc_num;
 
-CREATE VIEW prof_total_hours AS
-SELECT prof_acronym, SUM(class_hours*availability_percent*0.01) as total_hours
-FROM temp_dsd_main_info
-GROUP BY prof_acronym
-
-CREATE VIEW dsd_main_info AS
+CREATE VIEW temp2 AS
 SELECT uc_acronym, uc_name, professors.acronym AS director_acronym, students_estimate,
 component, class_hours, availability_percent, 
-temp_dsd_main_info.prof_acronym, temp_dsd_main_info.prof_name, total_hours
-FROM professors, temp_dsd_main_info, prof_total_hours
-WHERE temp_dsd_main_info.director=professors.id
-AND professors.acronym = prof_total_hours.prof_acronym
+temp1.prof_acronym, temp1.prof_name
+FROM professors, temp1
+WHERE temp1.director=professors.id;
 
--- NOTE: REVIEW VIEW TEMPDSDMAININFO
+CREATE VIEW prof_total_hours AS
+SELECT prof_acronym, SUM(class_hours*availability_percent*0.01) as total_hours
+FROM temp1
+GROUP BY prof_acronym;
+
+CREATE VIEW dsd_main_info AS
+SELECT uc_acronym, uc_name, prof_total_hours.prof_acronym AS director_acronym, students_estimate,
+component, class_hours, availability_percent, 
+temp2.prof_acronym, temp2.prof_name, prof_total_hours.total_hours
+FROM temp2
+LEFT OUTER JOIN prof_total_hours ON temp2.prof_acronym = prof_total_hours.prof_acronym;
