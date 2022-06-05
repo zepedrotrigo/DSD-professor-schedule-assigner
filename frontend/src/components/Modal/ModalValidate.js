f503c31a9cfe5a6317505272d74be81bf9299ea6
rezeett <edu_fernandes_2001@live.com.pt>
2022-06-05 19:33:19 +0100

    validate button working

---
+++ frontend/src/components/Modal/ModalValidate.js
@@ -0,0 +1,48 @@
+import "./ModalValidate.css";
+import React, { useState } from 'react';
+
+function ModalValidate(props) {
+
+    function hideModal(){
+        props.changeModal();
+    }
+
+    function loadWarnings(){
+        let result = [];
+        Array.from(props.warnings.warnings.entries()).map((entry) => {
+            const [k, v] = entry
+            console.log(v);
+            if (isNaN(v.total_hours)){
+                result.push(<li>Aula não atribuída: {v.prof_acronym} - {v.total_hours}</li>)
+            }
+            else if(v.total_hours==null){
+                result.push(<li>Professor a não ser utilizado: {v.prof_acronym} - {v.total_hours} hours</li>);
+            }
+            else{
+                result.push(<li>Professor sobrecarregado: {v.prof_acronym} - {v.total_hours} hours</li>);
+            }
+        })
+        return(<>{result}</>);
+    }
+
+    return (
+        <>  
+            <div className="backdrop"></div>
+            <div className="modal">
+                <div>
+                    <h2>Avisos:</h2>
+                </div>
+                <div className="modal-content">
+                    <ul>
+                        {loadWarnings()}
+                    </ul>
+                </div>
+                <div className="modal-buttons">
+                    <button className="back-button" onClick={hideModal}>Voltar</button>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default ModalValidate;
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,12 +1,16 @@
 import "./Navbar.css";
 import { useNavigate, useLocation } from "react-router-dom";
 import { ReactComponent as YourSvg } from './ua-logo.svg';
+import ModalValidate from "./Modal/ModalValidate";
+import { useState } from "react";
 
 function Navbar(props) {
 
     const navigate = useNavigate();
     const location = useLocation();
     const ids = window.profsIdsAndNames;
+    const [showModal, setShowModal] = useState(false);
+    const [warnings, setWarnings] = useState(null);
 
     function loadNavComponents(){
         let result=[];
@@ -24,23 +28,53 @@ function Navbar(props) {
         props.onReload();
     }
 
+    function changeModal(){
+        setShowModal(!showModal);
+    }
+
     function changeAcronym(){
         navigate('/change-acronym', { state: {profs: ids} });
     }
 
+    function goHome(){
+        navigate('/');
+    }
+
+    function validateDSD(){
+        var max_hours=8;
+        fetch(`http://localhost:8000/v1/validate_dsd/?max_hours=${max_hours}`)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            setWarnings(data);
+            setShowModal(!showModal);
+        })
+    }
+
+    function exportDSD(){
+        fetch(`http://localhost:8000/v1/export_dsd/?file_type=json`)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+        })
+    }
+
     return (
-        <div className="navbar">
-            <div className="ua-logo">
-                <YourSvg className="ua-logo-img" />
-                <span className="logo-legend">dsd</span>
+        <>
+            {showModal && <ModalValidate changeModal={changeModal} warnings={warnings}/>}
+            <div className="navbar">
+                <div className="ua-logo" onClick={goHome}>
+                    <YourSvg className="ua-logo-img" />
+                    <span className="logo-legend">dsd</span>
+                </div>
+                <ul className="navbar-items">
+                    {loadNavComponents()}
+                    <li className="nav-item" onClick={validateDSD}>Validar</li>
+                    <li className="nav-item" onClick={exportDSD}>Exportar</li>
+                    <i style={{fontSize: '18px'}} className="fa" onClick={handleClick}>&#xf021;</i>
+                </ul>
             </div>
-            <ul className="navbar-items">
-                {loadNavComponents()}
-                <li className="nav-item">Validar</li>
-                <li className="nav-item">Exportar</li>
-                <i style={{fontSize: '18px'}} className="fa" onClick={handleClick}>&#xf021;</i>
-            </ul>
-        </div>
+        </>
     )
 }
 
