9aee0c5e2b3b78fd0d8714ca61d54d5076d356be
zepedrotrigo <josetrigo2001@gmail.com>
2022-06-05 06:08:53 -0700

    added count_classes view

--- backend/db/src/StoredProcedures.sql
+++ backend/db/src/StoredProcedures.sql
@@ -209,3 +209,19 @@ END $$
 
 DELIMITER ;
 -- CALL SortProfessors("prof_acronym DESC");
+
+-- DROP PROCEDURE SortUcs;
+DELIMITER $$
+
+CREATE PROCEDURE SortUcs(params VARCHAR(50))
+BEGIN
+    SELECT * FROM classes_main_panel_info
+    ORDER BY
+        CASE WHEN params LIKE 'prof_acronym asc' THEN prof_acronym END ASC ,
+        CASE WHEN params LIKE 'prof_acronym desc' THEN prof_acronym END DESC,
+        CASE WHEN params LIKE 'total_hours asc' THEN total_hours END ASC,
+        CASE WHEN params LIKE 'total_hours desc' THEN total_hours END DESC;
+END $$
+
+DELIMITER ;
+-- CALL SortUcs("prof_acronym DESC");
