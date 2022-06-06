379f3508bfb44ee49eaced21a018ccd6c5ef4124
pedromonteiro01 <pmapm@ua.pt>
2022-06-06 11:31:08 +0100

    finish export dsd

--- frontend/src/components/Modal/components/ExportContent.js
+++ frontend/src/components/Modal/components/ExportContent.js
@@ -5,9 +5,9 @@ const ExportContent = (props) => {
 
     return (
         <div className='export-content'>
-            <Button class="edit-button" text="JSON" />
-            <Button class="edit-button" text="CSV" />
-            <Button class="edit-button" text="XLS" />
+            <Button class="edit-button" text="JSON" onClick={props.onJSONClick} />
+            <Button class="edit-button" text="CSV" onClick={props.onCSVClick} />
+            <Button class="edit-button" text="XLS" onClick={props.onXLSClick} />
         </div>
     );
 }
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,95 +1,121 @@
 import "./Navbar.css";
 import { useNavigate, useLocation } from "react-router-dom";
-import { ReactComponent as YourSvg } from './ua-logo.svg';
+import { ReactComponent as YourSvg } from "./ua-logo.svg";
 import Modal from "./Modal/Modal";
 import { useState } from "react";
 import ValidateContent from "./Modal/components/ValidateContent";
 import ExportContent from "./Modal/components/ExportContent";
 
 function Navbar(props) {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const ids = window.profsIdsAndNames;
+  const [showValidateModal, setShowValidateModal] = useState(false);
+  const [showExportModal, setShowExportModal] = useState(false);
+  const [warnings, setWarnings] = useState(null);
 
-    const navigate = useNavigate();
-    const location = useLocation();
-    const ids = window.profsIdsAndNames;
-    const [showValidateModal, setShowValidateModal] = useState(false);
-    const [showExportModal, setShowExportModal] = useState(false);
-    const [warnings, setWarnings] = useState(null);
+  function loadNavComponents() {
+    let result = [];
 
-    function loadNavComponents(){
-        let result=[];
+    if (location.pathname == "/change-acronym") {
+      result.push(<li className="on-page">Alterar siglas</li>);
+    } else
+      result.push(
+        <li className="nav-item" onClick={changeAcronym}>
+          Alterar siglas
+        </li>
+      );
 
-        if(location.pathname=="/change-acronym"){
-            result.push(<li className="on-page">Alterar siglas</li>);
-        }
-        else
-            result.push(<li className="nav-item" onClick={changeAcronym}>Alterar siglas</li>);
+    return <>{result}</>;
+  }
 
-        return(<>{result}</>)
-    }
+  function handleClick() {
+    props.onReload();
+  }
 
-    function handleClick(){
-        props.onReload();
-    }
+  function changeValidateModal() {
+    setShowValidateModal(!showValidateModal);
+  }
 
-    function changeValidateModal(){
+  function changeExportModal() {
+    setShowExportModal(!showExportModal);
+  }
+
+  function changeAcronym() {
+    navigate("/change-acronym", { state: { profs: ids } });
+  }
+
+  function goHome() {
+    navigate("/");
+  }
+
+  function validateDSD() {
+    var max_hours = 8;
+    fetch(`http://localhost:8000/v1/validate_dsd/?max_hours=${max_hours}`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setWarnings(data);
         setShowValidateModal(!showValidateModal);
-    }
+      });
+  }
 
-    function changeExportModal(){
+  function exportDSD(type) {
+      if (type==="json") {
+        window.location.href =
+        "http://localhost:8000/v1/export_dsd/?file_type=json";
         setShowExportModal(!showExportModal);
-    }
+      } else if (type==="csv"){
+        window.location.href = "http://localhost:8000/v1/export_dsd/?file_type=csv";
+        setShowExportModal(!showExportModal);
+      } else if (type==="xls") {
+        window.location.href = "http://localhost:8000/v1/export_dsd/?file_type=xls";
+        setShowExportModal(!showExportModal);
+      } else {
+          console.log("bad type");
+      }
+  }
 
-    function changeAcronym(){
-        navigate('/change-acronym', { state: {profs: ids} });
-    }
+  return (
+    <>
+      {showValidateModal && (
+        <Modal changeModal={changeValidateModal}>
+          <ValidateContent
+            changeModal={changeValidateModal}
+            warnings={warnings}
+          />
+        </Modal>
+      )}
 
-    function goHome(){
-        navigate('/');
-    }
-
-    function validateDSD(){
-        var max_hours=8;
-        fetch(`http://localhost:8000/v1/validate_dsd/?max_hours=${max_hours}`)
-        .then((response) => response.json())
-        .then((data) => {
-            console.log(data);
-            setWarnings(data);
-            setShowValidateModal(!showValidateModal);
-        })
-    }
-
-    function exportDSD(){
-        fetch(`http://localhost:8000/v1/export_dsd/?file_type=json`)
-        .then((response) => response.json())
-        .then((data) => {
-            setShowExportModal(!showExportModal)
-            console.log(data);
-        })
-    }
-
-    return (
-        <>
-            {showValidateModal && <Modal changeModal={changeValidateModal}>
-                 <ValidateContent changeModal={changeValidateModal} warnings={warnings} />
-                </Modal>}
-
-            {showExportModal && <Modal>
-                    <ExportContent />
-                </Modal>}
-            <div className="navbar">
-                <div className="ua-logo" onClick={goHome}>
-                    <YourSvg className="ua-logo-img" />
-                    <span className="logo-legend">dsd</span>
-                </div>
-                <ul className="navbar-items">
-                    {loadNavComponents()}
-                    <li className="nav-item" onClick={validateDSD}>Validar</li>
-                    <li className="nav-item" onClick={exportDSD}>Exportar</li>
-                    <i style={{fontSize: '18px'}} className="fa" onClick={handleClick}>&#xf021;</i>
-                </ul>
-            </div>
-        </>
-    )
+      {showExportModal && (
+        <Modal changeModal={changeExportModal}>
+          <ExportContent
+            onJSONClick={exportDSD("json")}
+            onCSVClick={exportDSD("csv")}
+            onXLSClick={exportDSD("xls")}
+          />
+        </Modal>
+      )}
+      <div className="navbar">
+        <div className="ua-logo" onClick={goHome}>
+          <YourSvg className="ua-logo-img" />
+          <span className="logo-legend">dsd</span>
+        </div>
+        <ul className="navbar-items">
+          {loadNavComponents()}
+          <li className="nav-item" onClick={validateDSD}>
+            Validar
+          </li>
+          <li className="nav-item" onClick={changeExportModal}>
+            Exportar
+          </li>
+          <i style={{ fontSize: "18px" }} className="fa" onClick={handleClick}>
+            &#xf021;
+          </i>
+        </ul>
+      </div>
+    </>
+  );
 }
 
 export default Navbar;
