70a448e6d56b6f89c094930077a94119bee70251
zepedrotrigo <josetrigo2001@gmail.com>
2022-06-22 17:43:52 +0100

    updated service ports

--- backend/api/controller.py
+++ backend/api/controller.py
@@ -1,5 +1,5 @@
 import mysql.connector, traceback, time
-import crud
+import service
 from typing import Optional
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
@@ -49,7 +49,7 @@ def get_classes(id: Optional[int] = -1, year: Optional[int]= -1, uc_id: Optional
 
     reset_cursor()
     with connection.cursor() as cursor:
-        return crud.get_classes(cursor, id, year, uc_id, component, hours, prof_id)
+        return service.get_classes(cursor, id, year, uc_id, component, hours, prof_id)
 
 @app.get("/v1/departments/")
 def get_departments(id: Optional[int] = -1, acronym: Optional[str] = "NULL", name: Optional[str] = "NULL", address: Optional[str] = "NULL", phone: Optional[str] = "NULL"):
@@ -57,7 +57,7 @@ def get_departments(id: Optional[int] = -1, acronym: Optional[str] = "NULL", nam
 
     reset_cursor()
     with connection.cursor() as cursor:
-        return crud.get_departments(cursor, id, acronym, name, address, phone)
+        return service.get_departments(cursor, id, acronym, name, address, phone)
 
 @app.get("/v1/professors/")
 def get_professors(prof_id: Optional[int] = -1, nmec: Optional[int] = -1, email: Optional[str] = "NULL", phone: Optional[str] = "NULL", acronym: Optional[str] = "NULL", name: Optional[str] = "NULL",rank: Optional[str]= "NULL",situation: Optional[str] = "NULL", department: Optional[int] = -1):
@@ -65,7 +65,7 @@ def get_professors(prof_id: Optional[int] = -1, nmec: Optional[int] = -1, email:
 
     reset_cursor()
     with connection.cursor() as cursor:
-        return crud.get_professors(cursor, prof_id, nmec, email, phone, acronym, name, rank, situation, department)
+        return service.get_professors(cursor, prof_id, nmec, email, phone, acronym, name, rank, situation, department)
 
 @app.get("/v1/dsders/")
 def get_dsders(id: Optional[int] = -1):
@@ -73,7 +73,7 @@ def get_dsders(id: Optional[int] = -1):
 
     reset_cursor()
     with connection.cursor() as cursor:
-        return crud.get_dsders(cursor, id)
+        return service.get_dsders(cursor, id)
 
 @app.get("/v1/courses/")
 def get_courses(id: Optional[int] = -1, acronym: Optional[str] = "NULL", name: Optional[str] = "NULL", department: Optional[int] = -1, director: Optional[int] = -1):
@@ -81,7 +81,7 @@ def get_courses(id: Optional[int] = -1, acronym: Optional[str] = "NULL", name: O
 
     reset_cursor()
     with connection.cursor() as cursor:
-        return crud.get_courses(cursor, id, acronym, name, department, director)
+        return service.get_courses(cursor, id, acronym, name, department, director)
 
 @app.get("/v1/ucs/")
 def get_ucs(id: Optional[int] = -1, acronym: Optional[str] = "NULL", name: Optional[str] = "NULL", director: Optional[int] = -1):
@@ -89,7 +89,7 @@ def get_ucs(id: Optional[int] = -1, acronym: Optional[str] = "NULL", name: Optio
 
     reset_cursor()
     with connection.cursor() as cursor:
-        return crud.get_ucs(cursor, id, acronym, name, director)
+        return service.get_ucs(cursor, id, acronym, name, director)
 
 @app.get("/v1/wishlists/")
 def get_wishlists(id: Optional[int] = -1, year: Optional[int] = -1, prof_id: Optional[int] = -1, class_id: Optional[int] = -1):
@@ -97,7 +97,7 @@ def get_wishlists(id: Optional[int] = -1, year: Optional[int] = -1, prof_id: Opt
 
     reset_cursor()
     with connection.cursor() as cursor:
-        return crud.get_wishlists(cursor, id, year, prof_id, class_id)
+        return service.get_wishlists(cursor, id, year, prof_id, class_id)
 
 @app.get("/v1/classes_main_panel_info/")
 def classes_main_panel_info(params: Optional[str] = "unassigned_classes desc"):
@@ -105,7 +105,7 @@ def classes_main_panel_info(params: Optional[str] = "unassigned_classes desc"):
 
     reset_cursor()
     with connection.cursor() as cursor:
-        return crud.classes_main_panel_info(cursor, params)
+        return service.classes_main_panel_info(cursor, params)
 
 @app.get("/v1/professors_main_panel_info/")
 def professors_main_panel_info(params: Optional[str] = "total_hours asc", prof_ids: Optional[str] = ""):
@@ -113,7 +113,7 @@ def professors_main_panel_info(params: Optional[str] = "total_hours asc", prof_i
 
     reset_cursor()
     with connection.cursor() as cursor:
-        return crud.professors_main_panel_info(cursor, params, prof_ids)
+        return service.professors_main_panel_info(cursor, params, prof_ids)
 
 @app.get("/v1/prof_total_hours/")
 def get_prof_total_hours():
@@ -121,7 +121,7 @@ def get_prof_total_hours():
 
     reset_cursor()
     with connection.cursor() as cursor:
-        return crud.get_prof_total_hours(cursor)
+        return service.get_prof_total_hours(cursor)
 
 @app.put("/v1/classes/")
 def assign_prof_to_class(class_id: int, prof_id: int):
@@ -129,7 +129,7 @@ def assign_prof_to_class(class_id: int, prof_id: int):
 
     reset_cursor()
     with connection.cursor() as cursor:
-        return crud.assign_prof_to_class(connection, cursor, class_id, prof_id)
+        return service.assign_prof_to_class(connection, cursor, class_id, prof_id)
 
 @app.put("/v1/professors/")
 def update_prof_acronym(prof_id: int, acronym: str):
@@ -137,7 +137,7 @@ def update_prof_acronym(prof_id: int, acronym: str):
 
     reset_cursor()
     with connection.cursor() as cursor:
-        return crud.update_prof_acronym(connection, cursor, prof_id, acronym)
+        return service.update_prof_acronym(connection, cursor, prof_id, acronym)
 
 @app.get("/v1/validate_dsd/")
 def validate_dsd(max_hours: int):
@@ -145,7 +145,7 @@ def validate_dsd(max_hours: int):
 
     reset_cursor()
     with connection.cursor() as cursor:
-        return crud.validate_dsd(cursor, max_hours)
+        return service.validate_dsd(cursor, max_hours)
 
 @app.get("/v1/export_dsd/")
 def export_dsd(file_type: str):
@@ -153,6 +153,6 @@ def export_dsd(file_type: str):
 
     reset_cursor()
     with connection.cursor() as cursor:
-        filepath = crud.export_dsd(cursor, file_type)
+        filepath = service.export_dsd(cursor, file_type)
 
     return FileResponse(path=filepath, filename=filepath)
